9aadb7edd803a142dc1af72ef7cf69344195280d
The179UCETile <khang240582@gmail.com>
2026-07-29 12:11:05 +0700

    This

---
+++ demo.js
@@ -0,0 +1,17 @@
+function format(e) {
+  let decim = new Decimal(e);
+  document.getElementById("results").innerHTML = `Grand Button Incremental: ${BadNotations.GrandButtonIncrementalStandard.format(decim)}<br>
+Old setsumi standard notation: ${BadNotations.OldSetsumiStandard.format(decim)}<br>
+True 179uc notation: ${BadNotations.True179ucStandard.format(decim)}<br>
+Vector's standard notation: ${BadNotations.VectorStandard.format(decim)}<br>
+New vector's standard notation: ${BadNotations.NewVectorStandard.format(decim)}<br>
+Ultimer's notation: ${BadNotations.Ultimer.format(decim)}<br>
+Merging Legends: ${BadNotations.MergingLegendsStandard.format(decim)}<br>
+Denutation: ${BadNotations.Denutation.format(decim, {max: "ee100"})}<br>
+Shit standard: ${BadNotations.CrapStandard.format(decim)}<br>
+Points Progression: ${BadNotations.PointsProgressionStandard.format(decim)}`
+}
+document.getElementById("num").addEventListener("input", () => {
+  format(document.getElementById("num").value)
+});
+format("1");
