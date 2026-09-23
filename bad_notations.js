ca86f3e7e4be8d5bcb0ae444ae16949184635b9c
The179UCETile <the179ucetile@proton.me>
2026-09-23 20:48:22 +0700

    Update bad_notations.js

--- bad_notations.js
+++ bad_notations.js
@@ -747,7 +747,7 @@ function pmn(n, config) {
 		};
 		return s.length > config.maxChars ? `${s.slice(0, config.maxChars - 3)}...` : s;
 	} else {
-		return `[${pmn(n.slog(config.base).sub("1"))}]${pmn(Decimal.iteratedexp(config.base, "2", n.slog().mod("1")))}`
+		return `[${pmn(n.slog(config.base).sub("2"))}]${pmn(Decimal.iteratedexp(config.base, "3", n.slog().mod("1")))}`
 	}
 }
 return {
